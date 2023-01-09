762fc009cd1a8ef14daf36529080ddada0d210e2
Bryce Mecum <petridish@gmail.com>
2023-01-08 15:18:57 -0900

    Apparently Nuxt can't handle <p> tags?

--- pages/index.vue
+++ pages/index.vue
@@ -4,8 +4,7 @@
     Fighting evil and tyranny, with all his power, and with all of his might. Ulysses — no-one else can do the things
     you
     do. Ulysses — like a bolt of thunder from the blue. Ulysses — always fighting all the evil forces bringing peace and
-    justice to all.
-  </p>
+    justice to all.</p>
 
   <h3>Header 3</h3>
   <ul>
@@ -22,30 +21,29 @@
   </div>
 
   <h3>Forms</h3>
-  <p>
+
   <h4>Input</h4>
   <form>
-    <label>Label<input type="text" /></label>
-    <input type="submit" />
+    <label>Label<input type="text"></label>
+    <input type="submit">
   </form>
-  </p>
-  <p>
+
+
   <h4>Textarea</h4>
 
   <form>
     <label>Label <textarea rows="8" cols="40"></textarea></label>
   </form>
-  </p>
+
 
   <p>This is my boss, Jonathan Hart, a self-made millionaire, he's quite a guy. This is Mrs H., she's gorgeous,
     she's one
     lady who knows how to take care of herself. By the way, my name is Max. I take care of both of them, which ain't
     easy,
-    'cause when they met it was MURDER!
-  </p>
+    'cause when they met it was MURDER!</p>
+
   <p>There's a voice that keeps on calling me. Down the road, that's where I'll always be. Every stop I make, I make a
     new
     friend. Can't stay for long, just turn around and I'm gone again. Maybe tomorrow, I'll want to settle down, Until
-    tomorrow, I'll just keep moving on.
-  </p>
+    tomorrow, I'll just keep moving on.</p>
 </template>
