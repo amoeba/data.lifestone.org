2cc9f137d2c401bab56fe25e88388fb66a6ee99f
Bryce Mecum <petridish@gmail.com>
2023-01-09 20:04:22 -0900

    Add icon to header

--- components/Header.vue
+++ components/Header.vue
@@ -1,12 +1,17 @@
 <template>
   <header>
     <div class="header-top">
-      <h1>
-        <NuxtLink to="/">data</NuxtLink>
-        <NuxtLink to="/">.lifestoned.org</NuxtLink>
-      </h1>
-      <div class="search-container">
-        <input class="search" type="text" placeholder="Search" />
+      <div class="header-left">
+        <Icon />
+        <h1>
+          <NuxtLink to="/">data</NuxtLink>
+          <NuxtLink to="/">.lifestoned.org</NuxtLink>
+        </h1>
+      </div>
+      <div class="header-right">
+        <div class="search-container">
+          <input class="search" type="text" placeholder="Search" />
+        </div>
       </div>
     </div>
     <Nav />
---
+++ components/icon.vue
@@ -0,0 +1,34 @@
+<script type="ts">
+export default {
+  data() {
+    const width = 16;
+    const dim = 4;
+    const tile_width = width / dim;
+    const base_color = "0, 170, 255"; // r, g, b
+
+    const tiles = [...Array(dim * dim).keys()].map(i => {
+      return {
+        i: i,
+        x: tile_width * (i % dim),
+        y: tile_width * (Math.floor(i / dim)),
+        color: "rgba(" + base_color + "," + Math.random() + ")"
+      }
+    });
+
+    return {
+      width: width,
+      viewBox: "0 0 " + width + " " + width,
+      tile_width: tile_width,
+      height: 10,
+      tiles: tiles
+    };
+  }
+}
+</script>
+
+<template>
+  <svg :width="width" :height="width" :viewBox="viewBox" xmlns="http://www.w3.org/2000/svg">
+    <rect v-for="tile in tiles" :v-key="tile.i" :x="tile.x" :y="tile.y" :width="tile_width" :height="tile_width"
+      :fill="tile.color" />
+  </svg>
+</template>
